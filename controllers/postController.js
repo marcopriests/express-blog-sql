c82d08ae3126e22f86bb9ce1169dde807cdb9acb
marcopriests <prtmrc98@gmail.com>
2025-05-28 16:21:12 +0200

    [UPD] milestone 3 fatta

--- controllers/postController.js
+++ controllers/postController.js
@@ -13,7 +13,15 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-    res.send(`Dettaglio del post ${req.params.id}`)
+    const id = req.params.id
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (results.length === 0) return res.status(404).json({ error: 'Post not found' })
+
+        res.json(results[0])
+    })
 }
 
 module.exports = { index, show }
